7c251e278619a62224622e912dca4cc5609727d7
Julien ADAMEK <julien.adamek@suse.com>
2022-03-02 08:58:16 +0100

    Fix epinio installation

--- gke-box/epinio/install.sh
+++ gke-box/epinio/install.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
 figlet epinio
-curl --silent --output epinio --location https://github.com/epinio/epinio/releases/download/v$1/epinio-linux-amd64
+curl --silent --output epinio --location https://github.com/epinio/epinio/releases/download/v$1/epinio-linux-x86_64
 sudo chmod +x epinio
 sudo mv epinio /usr/local/bin
